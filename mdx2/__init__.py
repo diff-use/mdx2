cc681a66eb00684b6b50911841d318409425bc54
Steve P. Meisburger <spmeisburger@gmail.com>
2025-08-14 19:52:32 -0400

    remove unnecessary getVersionNumber function

--- mdx2/__init__.py
+++ mdx2/__init__.py
@@ -1,9 +1,3 @@
-import pkg_resources
+from importlib.metadata import version
 
-
-def getVersionNumber():
-    version = pkg_resources.require("mdx2")[0].version
-    return version
-
-
-__version__ = getVersionNumber()
+__version__ = version("mdx2")
--- tests/test_version_number.py
+++ tests/test_version_number.py
@@ -1,12 +1,11 @@
 import re
 
-from mdx2 import getVersionNumber
+from mdx2 import __version__ as mdx2_version
 
 
-def test_version_number():
-    """version string returned by getVersionNumber should match symver semantics"""
-    version_string = getVersionNumber()
+def test_mdx2_version_number():
+    """check that mdx2.__version__ matches semver semantics"""
     semver_regex = r"^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$"  # noqa: E501
-    match = re.match(semver_regex, version_string)
+    match = re.match(semver_regex, mdx2_version)
     assert match
     major, minor, patch, prerelease, buildmetadata = match.groups()
