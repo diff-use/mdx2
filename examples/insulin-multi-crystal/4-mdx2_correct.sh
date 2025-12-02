ea0944568e4ac196952461e206a69e921ee7f41c
Steve Meisburger <spm82@cornell.edu>
2025-12-01 21:19:28 -0500

    updated multi-crystal example

--- examples/insulin-multi-crystal/4-mdx2_correct.sh
+++ examples/insulin-multi-crystal/4-mdx2_correct.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 set -e
 
+NPROC=64 # modify as needed
+
 # process background files
 cd mdx2
 for sub in {1..2}_bkg; do
     mkdir -p $sub
     cd $sub
     EXPTFILE="../../dials/${sub}/imported.expt"
-    mdx2.import_data $EXPTFILE --chunks 10 211 493 --nproc 5
-    mdx2.bin_image_series data.nxs 10 20 20 --valid_range 0 200 --outfile binned.nxs
+    mdx2.import_data $EXPTFILE --nproc $NPROC
+    mdx2.bin_image_series data.nxs 10 20 20 --valid_range 0 200 --outfile binned.nxs --nproc $NPROC
     cd ..
 done
 
